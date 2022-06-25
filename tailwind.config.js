96a3da1aabfee115a4ffed0c198e7845d255d216
Farhaan Khan <farhaankhanfiroz@gmail.com>
2022-06-25 13:35:31 +0530

    Add global font and styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+    },
+    fontFamily: {
+      'heading': ['Space Grotesk', 'sans-serif'],
+      'body': ['Inter', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        'app-yellow': '#FFF033',
+        'app-green': {
+          100: '#DDFAED',
+          200: '#22B573',
+          300: '#226F54',
+          400: '#002F32',
+        }
+      },
+    },
   },
   plugins: [],
 };
